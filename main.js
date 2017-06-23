7064a90faadd62b83af3c44356989b3bb980dfe4
Steve Licata <Steve.Licata@gmail.com>
2017-06-23 14:41:03 -0400

    updates

--- main.js
+++ main.js
@@ -14,18 +14,11 @@ window.onload = function() {
   var wins = document.getElementById('wins');
   var losses = document.getElementById('losses');
   var gameScore = 0;
-
-  //game numbers variable
-  // var num = [
-  //   1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100
-  // ];
-
-  //game numbers varialble
   var num = [
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10
   ];
 
-  //'random' variable generates random number, holds random number
+
   var random = num[Math.floor(Math.random() * num.length)];
   console.log("random number is " + random);
   //hides number from DOM
@@ -102,10 +95,8 @@ window.onload = function() {
     } else {
       console.log("else");
     }
-
-
+    
   }); //end user guess click
-
 
 
 }; //end window onload
